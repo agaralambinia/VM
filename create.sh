520d6721853ef2148cc66437d42c15d0ef85cfa7
Daria Efimova <agaralambinia@yandex.ru>
2024-03-09 06:26:21 +0400

    v2

--- create.sh
+++ create.sh
@@ -1,7 +1,17 @@
-systemctl enable --now libvirtd;
-virt-install --name=debian \
---vcpus=1 \
---memory=2048 \
---cdrom=/home/agara/Downloads/debian-12.4.0-amd64-netinst.iso \
---disk size=30.8 \
---os-variant=debian12
+#!/bin/bash
+
+# Download debian.iso
+if [ ! -f ./debian.iso ]; then
+    wget https://www.debian.org/download#:~:text=debian%2D12.4.0%2Damd64%2Dnetinst.iso -O debian.iso
+fi
+
+#Create VM
+#basefolder - папка для конфигурации ВМ, в школе это goinfree
+VBoxManage createvm --name debian42 --ostype "Debian_64" --register --basefolder /Users/agaralambinia/vms/ #--password-id password-id
+#Set memory and network
+VBoxManage modifyvm debian42 --memory 1024 --vram 128 --cpus 1
+#Create Disk and connect Debian Iso
+VBoxManage createmedium --filename /Users/agaralambinia/debian42/disk.vdi --size 315392 --format VDI
+
+#Start the VM
+VBoxHeadless --startvm debian42
